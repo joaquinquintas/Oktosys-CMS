a28f92c6d2a1755235c477c9553fd026d10a5db1
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-20 02:12:19 -0400

    wrote Highlight model

--- myproject/highlights/models.py
+++ myproject/highlights/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Highlight(models.Model):
+    highlights = models.BooleanField()
+    headline = models.CharField(max_length=255)
+    description = models.TextField()
+    icon_text = models.CharField(max_length=255)
