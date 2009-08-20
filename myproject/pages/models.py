7bd921b6e08f33b396660de209966aaba28d44a8
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-20 03:52:16 -0400

    pages.models.Page.__unicode__: return page title

--- myproject/pages/models.py
+++ myproject/pages/models.py
@@ -6,3 +6,7 @@ class Page(models.Model):
     html = models.TextField()
     parent = models.ForeignKey('self', blank=True, null=True,
                                        related_name='children')
+    
+    def __unicode__(self):
+        return self.title
+    
