0f57031cd24bd428ca14777d1e2380ff085f76eb
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-20 02:23:01 -0400

    not clear on what Highlight.highlights column is for, so removing for now

--- myproject/highlights/models.py
+++ myproject/highlights/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Highlight(models.Model):
-    highlights = models.BooleanField()
     headline = models.CharField(max_length=255)
     description = models.TextField()
     icon_text = models.CharField(max_length=255)
