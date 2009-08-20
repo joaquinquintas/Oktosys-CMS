73e2adaded56cd8d9c76ab65467b6d2ead8e79e0
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-20 03:11:11 -0400

    added a slot column to Highlight model

--- myproject/highlights/models.py
+++ myproject/highlights/models.py
@@ -3,12 +3,16 @@ from django.db import models
 import os
 from django.template.defaultfilters import slugify
 
+# how many slots are there supposed to be?
+slot_choices = ((x + 1, str(x + 1)) for x in xrange(3))
+
 def generate_icon_filename(instance, old_filename):
     extension = os.path.splitext(old_filename)[1]
     filename = slugify(instance.headline) + '.' + extension
     return 'icons/' + filename
 
 class Highlight(models.Model):
+    slot = models.PositiveSmallIntegerField(choices=slot_choices)
     headline = models.CharField(max_length=255)
     description = models.TextField()
     icon = models.ImageField(upload_to=generate_icon_filename)
