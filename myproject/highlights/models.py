8ac348453178386e4eccde8ff4bb4da6a440f174
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-20 03:00:55 -0400

    add Highlight.icon (an image field)

--- myproject/highlights/models.py
+++ myproject/highlights/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+import os
+from django.template.defaultfilters import slugify
+
+def generate_icon_filename(instance, old_filename):
+    extension = os.path.splitext(old_filename)[1]
+    filename = slugify(instance.headline) + '.' + extension
+    return 'icons/' + filename
+
 class Highlight(models.Model):
     headline = models.CharField(max_length=255)
     description = models.TextField()
+    icon = models.ImageField(upload_to=generate_icon_filename)
     icon_text = models.CharField(max_length=255)
