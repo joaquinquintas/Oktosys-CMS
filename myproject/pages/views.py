a0126b23f48d3be9a5769cd6ac52f71c91c3a508
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-27 17:05:10 -0400

    ads: added 3 random to home page

--- myproject/pages/views.py
+++ myproject/pages/views.py
@@ -1,8 +1,11 @@
-from myproject.pages.models import *
-from myproject.news.models import *
+from myproject.pages.models import Page
+from myproject.news.models import Entry
+from myproject.ads.models import Ad
 from django.shortcuts import render_to_response
 
 def home(request):
     news = Entry.objects.order_by('-created_on')[:3]
+    ads = Ad.objects.order_by('?')[:3]
     
-    return render_to_response('pages/home.html', {'news_entries': news})
+    return render_to_response('pages/home.html',
+        {'news_entries': news, 'ads': ads})
