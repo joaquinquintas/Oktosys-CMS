d724a077f053df0e9f7679a343d0647df70366eb
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-27 17:45:03 -0400

    updated sponsors model

--- myproject/sponsors_panel/models.py
+++ myproject/sponsors_panel/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 import os
+from django.template.defaultfilters import slugify
 
 def generate_image_filename(instance, old_filename):
     extension = os.path.splitext(old_filename)[1]
-    filename = instance.page.slug + extension
+    filename = slugify(instance.title) + extension
     return 'sponsor/' + filename
 
 class Sponsor(models.Model):
+    name = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to=generate_image_filename)
+    link = models.URLField()
+    
+    def __unicode__(self):
+        return "Sponsor (%s, %s)" % (self.name, self.title)
+    
