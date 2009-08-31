7465081e2076bcc057687d0993f71328f0eefc82
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-31 22:20:44 +0400

    usermgr app: some modifications

--- myproject/usermgr/forms.py
+++ myproject/usermgr/forms.py
@@ -1,25 +1,23 @@
-#Organic registration
+from models import Rider
 from django import forms
 
-class RegistrationForm(models.Model):
-	username = forms.CharField(label=u'Username', max_length=50)
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Rider
+    
+    username = forms.CharField(label=u'Username', max_length=50)
 	password = forms.CharField(label=u'Password', widget=forms.PasswordInput)
+	name_first = forms.CharField(label=u'First name', max_length=150)
+	name_last = forms.CharField(label=u'Last name', max_length=150)
 	email = forms.EmailField(label=u'Email address')
-	firstName = forms.CharField(label=u'First Name', max_length=100)
-	lastName = forms.CharField(label=u'Last Name', max_length=100)
-	tel_main = forms.CharField(label=u'Telephone', max_length=100)
-	tel_mobile = forms.CharField(label=u'Mobile', max_length=100)
-	rcv_fbupdates = forms.BooleanField(label=u'Receive Facebook updates', widget=forms.CheckboxInput)
-	rcv_smsupdates = forms.BooleanField(label=u'Receive SMS updates', widget=forms.CheckboxInput)
-	rcv_emailupdates = forms.BooleanField(label=u'Receive email updates', widget=forms.CheckboxInput)
-	
-class EnewsletterForm(models.Model):
+	phone_main = forms.CharField(label=u'Telephone number', max_length=100)
+	phone_mobile = forms.CharField(label=u'Mobile number', max_length=100)
+	settings_updates_fb = forms.BooleanField(label=u'Receive updates via Facebook', widget=forms.CheckboxInput)
+	settings_updates_email = forms.BooleanField(label=u'Receive updates via email', widget=forms.CheckboxInput)
+	settings_updates_sms = forms.BooleanField(label=u'Receive updates via SMS', widget=forms.CheckboxInput)
+	settings_fb_post = forms.BooleanField(label=u'Post your activities to Facebook', widget=forms.CheckboxInput)
+	settings_share_profile = forms.BooleanField(label=u'Make your profile page private', widget=forms.CheckboxInput)
+
+class ENewsletterForm(forms.Form):
 	name = form.CharField(label=u'Name', max_length=100)
-	email = forms.EmailField(label=u'Email Addresss')
-
-
-	
-	
-	
-
-
+	email = forms.EmailField(label=u'Email address')
--- myproject/usermgr/models.py
+++ myproject/usermgr/models.py
@@ -1,93 +1,90 @@
 from django.db import models
 
-# Create your models here.
-
 class Rider(models.Model):
-	firstName = models.CharField(max_length=150)
-	lastName = models.CharField(max_length=150)
-	emailAdd = models.EmailField(unique=True)
-	tel_main = models.CharField(max_length=100)
-	tel_mobile = models.CharField(max_length=100)
+	name_first = models.CharField(max_length=150)
+	name_last = models.CharField(max_length=150)
+	email = models.EmailField(unique=True)
+	phone_main = models.CharField(max_length=100)
+	phone_mobile = models.CharField(max_length=100)
+	
 	username = models.CharField(max_length=50)
 	password = models.CharField(max_length=50)
+	
 	bike_make = models.CharField(max_length=255)
 	bike_model = models.CharField(max_length=255)
 	bike_link = models.URLField(verify_exists=True)
+    
+	settings_updates_fb = models.BooleanField(default=True)
+	settings_updates_email = models.BooleanField(default=True)
+	settings_updates_sms = models.BooleanField(default=True)
+	settings_fb_post = models.BooleanField(default=True)
+	settings_profile_private = models.BooleanField(default=False)
 
-class Rider_Settings(models.Model):
-	rider_id = models.ForeignKey('Rider')
-	recv_fbupdates = models.BooleanField()
-	recv_emailupdates = models.BooleanField()
-	recv_smsupdates = models.BooleanField()
-	facebook_live = models.BooleanField()
-	fb_ocbclink = models.BooleanField()
-	privacy = models.BooleanField()
+RACE_TYPES = (
+	('I50', '50KM, INDIVIDUAL'),
+	('I40', '40KM, INDIVIDUAL'),
+	('I20', '20KM, INDIVIDUAL'),
+	('I5', '5KM, INDIVIDUAL'),
+	('IC', 'CRITERIUM, INDIVIDUAL'),
+	('ITR', 'TRICYCLE, INDIVIDUAL'),
+	('S50', '50KM, STUDENT'),
+	('S40', '40KM, STUDENT'),
+	('S20', '20KM, STUDENT'),
+	('S5', '5KM, STUDENT'),
+	('SC', 'CRITERIUM, STUDENT'),
+	('STR', 'TRICYCLE, STUDENT'),
+	('TC50', '50KM, TEAM, CORPORATE'),
+	('TC40', '40KM, TEAM, CORPORATE'),
+	('TC20', '20KM, TEAM, CORPORATE'),
+	('TC5', '5KM, TEAM, CORPORATE'),
+	('TCC', 'CRITERIUM, TEAM, CORPORATE'),
+	('TCTR', 'TRICYCLE, TEAM, CORPORATE'),
+	('TS50', '50KM, TEAM, SCHOOL'),
+	('TS40', '40KM, TEAM, SCHOOL'),
+	('TS20', '20KM, TEAM, SCHOOL'),
+	('TS5', '5KM, TEAM, SCHOOL'),
+	('TSC', 'CRITERIUM, TEAM, SCHOOL'),
+	('TSTR', 'TRICYCLE, TEAM, SCHOOL'),
+	('TO50', '50KM, TEAM, OTHER'),
+	('TO40', '40KM, TEAM, OTHER'),
+	('TO20', '20KM, TEAM, OTHER'),
+	('TO5', '5KM, TEAM, OTHER'),
+	('TOC', 'CRITERIUM, TEAM, OTHER'),
+	('TOTR', 'TRICYCLE, TEAM, OTHER'),
+)
 
-class Race_Profile(models.Model):
-	RACE_TYPES = (
-		('I50', '50KM, INDIVIDUAL'),
-		('I40', '40KM, INDIVIDUAL'),
-		('I20', '20KM, INDIVIDUAL'),
-		('I5', '5KM, INDIVIDUAL'),
-		('IC', 'CRITERIUM, INDIVIDUAL'),
-		('ITR', 'TRICYCLE, INDIVIDUAL'),
-		('S50', '50KM, STUDENT'),
-		('S40', '40KM, STUDENT'),
-		('S20', '20KM, STUDENT'),
-		('S5', '5KM, STUDENT'),
-		('SC', 'CRITERIUM, STUDENT'),
-		('STR', 'TRICYCLE, STUDENT'),
-		('TC50', '50KM, TEAM, CORPORATE'),
-		('TC40', '40KM, TEAM, CORPORATE'),
-		('TC20', '20KM, TEAM, CORPORATE'),
-		('TC5', '5KM, TEAM, CORPORATE'),
-		('TCC', 'CRITERIUM, TEAM, CORPORATE'),
-		('TCTR', 'TRICYCLE, TEAM, CORPORATE'),
-		('TS50', '50KM, TEAM, SCHOOL'),
-		('TS40', '40KM, TEAM, SCHOOL'),
-		('TS20', '20KM, TEAM, SCHOOL'),
-		('TS5', '5KM, TEAM, SCHOOL'),
-		('TSC', 'CRITERIUM, TEAM, SCHOOL'),
-		('TSTR', 'TRICYCLE, TEAM, SCHOOL'),
-		('TO50', '50KM, TEAM, OTHER'),
-		('TO40', '40KM, TEAM, OTHER'),
-		('TO20', '20KM, TEAM, OTHER'),
-		('TO5', '5KM, TEAM, OTHER'),
-		('TOC', 'CRITERIUM, TEAM, OTHER'),
-		('TOTR', 'TRICYCLE, TEAM, OTHER'),
-	)
-	rider_id = models.ForeignKey('Rider')
+class Race(models.Model):
+	rider = models.ForeignKey('Rider')
 	registered = models.BooleanField()
-	race_type = models.CharField(max_length=4, choices =RACE_TYPES)
+	race_type = models.CharField(max_length=4, choices=RACE_TYPES)
 	team_id = models.ForeignKey('Team')
-	
+
+TEAM_TYPES = (
+    ('TC50', '50KM, TEAM, CORPORATE'),
+    ('TC40', '40KM, TEAM, CORPORATE'),
+    ('TC20', '20KM, TEAM, CORPORATE'),
+    ('TC5', '5KM, TEAM, CORPORATE'),
+    ('TCC', 'CRITERIUM, TEAM, CORPORATE'),
+    ('TCTR', 'TRICYCLE, TEAM, CORPORATE'),
+    ('TS50', '50KM, TEAM, SCHOOL'),
+    ('TS40', '40KM, TEAM, SCHOOL'),
+    ('TS20', '20KM, TEAM, SCHOOL'),
+    ('TS5', '5KM, TEAM, SCHOOL'),
+    ('TSC', 'CRITERIUM, TEAM, SCHOOL'),
+    ('TSTR', 'TRICYCLE, TEAM, SCHOOL'),
+    ('TO50', '50KM, TEAM, OTHER'),
+    ('TO40', '40KM, TEAM, OTHER'),
+    ('TO20', '20KM, TEAM, OTHER'),
+    ('TO5', '5KM, TEAM, OTHER'),
+    ('TOC', 'CRITERIUM, TEAM, OTHER'),
+    ('TOTR', 'TRICYCLE, TEAM, OTHER'),
+)
+
 class Team(models.Model):
-	TEAM_TYPES = (
-                ('TC50', '50KM, TEAM, CORPORATE'),
-                ('TC40', '40KM, TEAM, CORPORATE'),
-                ('TC20', '20KM, TEAM, CORPORATE'),
-                ('TC5', '5KM, TEAM, CORPORATE'),
-                ('TCC', 'CRITERIUM, TEAM, CORPORATE'),
-                ('TCTR', 'TRICYCLE, TEAM, CORPORATE'),
-                ('TS50', '50KM, TEAM, SCHOOL'),
-                ('TS40', '40KM, TEAM, SCHOOL'),
-                ('TS20', '20KM, TEAM, SCHOOL'),
-                ('TS5', '5KM, TEAM, SCHOOL'),
-                ('TSC', 'CRITERIUM, TEAM, SCHOOL'),
-                ('TSTR', 'TRICYCLE, TEAM, SCHOOL'),
-                ('TO50', '50KM, TEAM, OTHER'),
-                ('TO40', '40KM, TEAM, OTHER'),
-                ('TO20', '20KM, TEAM, OTHER'),
-                ('TO5', '5KM, TEAM, OTHER'),
-                ('TOC', 'CRITERIUM, TEAM, OTHER'),
-                ('TOTR', 'TRICYCLE, TEAM, OTHER'),
-        )
 	race_type = models.CharField(max_length=4, choices=TEAM_TYPES)
-	user1_id = models.ForeignKey('Rider')
-	user2_id = models.ForeignKey('Rider')
-	user3_id = models.ForeignKey('Rider')
-	user4_id = models.ForeignKey('Rider')
-	team_status = models.BooleanField()
+	user1 = models.ForeignKey('Rider')
+	user2 = models.ForeignKey('Rider')
+	user3 = models.ForeignKey('Rider')
+	user4 = models.ForeignKey('Rider')
+	status = models.BooleanField()
 	counter = models.IntegerField()
-
-
