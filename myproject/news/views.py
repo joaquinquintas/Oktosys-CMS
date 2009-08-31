21894d0c332799ca7649a7d6edc102124e0f94ba
Kenny Shen <kenny@oktosys.com>
2009-08-31 16:51:32 +0800

    add search box to base.html added template for news archive

--- myproject/news/views.py
+++ myproject/news/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from django.shortcuts import get_object_or_404, render_to_response
+from myproject.news.models import Entry
+
+
+def entries_index(request):
+        return render_to_response('news/news_index.html',
+                                        { 'entry_list': Entry.objects.all() })
+
