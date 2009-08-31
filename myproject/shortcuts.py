f9937014204574e066f0bea51134e206442f7fae
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-09-01 01:16:46 +0400

    shortcuts: auth riders

--- myproject/shortcuts.py
+++ myproject/shortcuts.py
@@ -1,6 +1,18 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from myproject.entourage import models
 
 def render(request, template, data={}):
     return render_to_response(template, data,
         context_instance=RequestContext(request))
+
+def auth(request):
+    rider_id = request.session['rider_id']
+    if not rider_id:
+        return False
+    else:
+        try:
+            return models.Rider(id=rider_id)
+            return rider
+        except Rider.DoesNotExist:
+            return False
