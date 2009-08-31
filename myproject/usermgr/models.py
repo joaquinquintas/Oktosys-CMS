e6a7c1e0129a3e8abc1edbfd75c7c4e23db77f47
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-31 22:45:38 +0400

    usermgr: moved forms to models.py

--- myproject/usermgr/models.py
+++ myproject/usermgr/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 class Rider(models.Model):
 	name_first = models.CharField(max_length=150)
@@ -88,3 +89,34 @@ class Team(models.Model):
 	user4 = models.ForeignKey('Rider')
 	status = models.BooleanField()
 	counter = models.IntegerField()
+
+class RegistrationForm(forms.ModelForm):
+    class Meta:
+        model = Rider
+        fields = ('name_first', 'name_last',
+            'email',
+            'username', 'password',
+            'phone_main', 'phone_mobile',
+            'bike_make', 'bike_model', 'bike_link',
+            'settings_updates_fb',
+            'settings_updates_email',
+            'settings_updates_sms',
+            'settings_fb_post',
+            'settings_profile_private',)
+    
+    username = forms.CharField(label=u'Username', max_length=50)
+	password = forms.CharField(label=u'Password', widget=forms.PasswordInput)
+	name_first = forms.CharField(label=u'First name', max_length=150)
+	name_last = forms.CharField(label=u'Last name', max_length=150)
+	email = forms.EmailField(label=u'Email address')
+	phone_main = forms.CharField(label=u'Telephone number', max_length=100)
+	phone_mobile = forms.CharField(label=u'Mobile number', max_length=100)
+	settings_updates_fb = forms.BooleanField(label=u'Receive updates via Facebook', widget=forms.CheckboxInput)
+	settings_updates_email = forms.BooleanField(label=u'Receive updates via email', widget=forms.CheckboxInput)
+	settings_updates_sms = forms.BooleanField(label=u'Receive updates via SMS', widget=forms.CheckboxInput)
+	settings_fb_post = forms.BooleanField(label=u'Post your activities to Facebook', widget=forms.CheckboxInput)
+	settings_share_profile = forms.BooleanField(label=u'Make your profile page private', widget=forms.CheckboxInput)
+
+class ENewsletterForm(forms.Form):
+	name = form.CharField(label=u'Name', max_length=100)
+	email = forms.EmailField(label=u'Email address')
