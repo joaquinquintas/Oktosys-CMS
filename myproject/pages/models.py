9cae7c54e51f56352f5afed266e5048a25d03dea
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-09-01 10:32:17 +0400

    pages/models.py: enabled TinyMCE

--- myproject/pages/models.py
+++ myproject/pages/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 class Page(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    html = models.TextField()
+    html = tinymce_models.HTMLField()
     parent = models.ForeignKey('self', blank=True, null=True,
                                        related_name='children')
     
