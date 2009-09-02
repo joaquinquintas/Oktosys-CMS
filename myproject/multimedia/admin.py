5166e4ce1f5ec8235bbbcc2971e45ee6ac3b59ca
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-09-03 00:11:03 +0400

    multimedia app: video model + admin ui

--- myproject/multimedia/admin.py
+++ myproject/multimedia/admin.py
@@ -1,7 +1,11 @@
-from models import Photo
+from models import Photo, Video
 from django.contrib import admin
 
 class PhotoAdmin(admin.ModelAdmin):
     pass
 
+class VideoAdmin(admin.ModelAdmin):
+    fields = ('url',)
+
 admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Video, VideoAdmin)
--- myproject/multimedia/models.py
+++ myproject/multimedia/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 from django import forms
 
+import gdata.youtube
+import gdata.youtube.service
+yt = gdata.youtube.service.YouTubeService()
+
+from django.conf import settings
+yt.developer_key = settings.YOUTUBE_KEY
+
+yt_regex = r'^((?:http://)?(?:\w+\.)?youtube\.com/(?:(?:v/)|(?:(?:watch(?:\.php)?)?\?(?:.+&)?v=)))?([0-9A-Za-z_-]+)(?(1).+)?$'
+
 import os
 import time
+import re
 
 def generate_filename(instance, old_filename):
     extension = os.path.splitext(old_filename)[1]
@@ -16,4 +26,26 @@ class Photo(models.Model):
     
     def __unicode__(self):
         return self.title
+    
+class Video(models.Model):
+    url = models.URLField()
+    title = models.CharField(max_length=255)
+    embed_url = models.CharField(max_length=255)
+    embed_type = models.CharField(max_length=255)
+    
+    def save(self):
+        try:
+            video_id = re.compile('.*\?v\=([^\\\/&]+)').match(self.url).groups()[0]
+            video = yt.GetYouTubeVideoEntry(video_id=video_id)
+            
+            self.title = video.media.title.text
+            self.embed_url = video.media.content[0].url
+            self.embed_type = video.media.content[0].type
+            
+            super(Video, self).save()
+        except:
+            pass
+    
+    def __unicode__(self):
+        return self.url
     
