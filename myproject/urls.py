3f7733f6ca76cd345af52ddcc52c24f87f0caba7
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-09-04 03:48:32 +0400

    urls.py: updated

--- myproject/urls.py
+++ myproject/urls.py
@@ -9,7 +9,6 @@ from myproject.search.views import search_site
 
 urlpatterns = patterns('',
     (r'^$', 'myproject.pages.views.home'),
-    (r'^fbconnect/', include('myproject.pyfacebook.urls')),
     (r'^pages/', include('myproject.pages.urls')),
     (r'^admin/filebrowser/', include('filebrowser.urls')),
     (r'^admin/', include(admin.site.urls)),
