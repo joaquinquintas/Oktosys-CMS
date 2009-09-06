b3942e7f811d373dbe68fa6fa1cf1d9b4628dbfb
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-09-06 16:33:34 +0400

    entourage: typo

--- myproject/entourage/urls.py
+++ myproject/entourage/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('myproject.entourage.views',
     url(r'login', 'login'),
     url(r'logout', 'logout'),
     url(r'profile/(\d+)', 'profile'),
-    url(r'edit_profile', 'edit_profile'),
+    url(r'edit_profile', 'profile_edit'),
     url(r'', 'home'),
 )
