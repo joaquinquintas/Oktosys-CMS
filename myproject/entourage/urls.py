ff4c07b3653c1c34e354b55f084ed6fa67acd4d2
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-09-06 11:22:52 +0400

    entourage: breadcrumbs

--- myproject/entourage/urls.py
+++ myproject/entourage/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('myproject.entourage.views',
     url(r'signup', 'signup'),
     url(r'login', 'login'),
     url(r'logout', 'logout'),
+    url(r'profile/(\d+)', 'profile'),
     url(r'', 'home'),
 )
