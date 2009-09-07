38b311f475b2dabac009a7a8f2cdb9f2a628599c
Kenny Shen <kenny@oktosys.com>
2009-09-08 00:56:00 +0800

    typo fix

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     (r'^multimedia/', include('myproject.multimedia.urls')),
     url(r'^search/', SearchView(), name="haystack_search"),
     (r'^xml/homeflash.xml', 'django.views.generic.simple.direct_to_template', {'template': 'pages/homeflash.xml'}),
-    (r'^enewsletter/', include('myproject.enewsletter.urls'),
+    (r'^enewsletter/', include('myproject.enewsletter.urls')),
 )
 if settings.DEBUG:
     urlpatterns += patterns('',
