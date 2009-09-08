49f1c8dded254cf63594d2ce16f1d65e4e815ef3
Kenny Shen <kenny@oktosys.com>
2009-09-08 10:39:41 +0800

    test

--- myproject/enewsletter/views.py
+++ myproject/enewsletter/views.py
@@ -6,8 +6,6 @@ def subscribe(request):
 	if request.method == 'POST':
 		form = EnewsForm(request.POST)
 		if form.is_valid:
-			form.name = form.cleaned_data['name']
-			form.email_add = form.cleaned_data['email_add']
 			form.save()
 			return HttpResponseRedirect('/subscribed/')
 		else:
