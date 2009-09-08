ab8bfb5f669af01e1a59ff4a19a24694ca37a016
Kenny Shen <kenny@oktosys.com>
2009-09-08 11:50:50 +0800

    amended

--- myproject/enewsletter/urls.py
+++ myproject/enewsletter/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from myproject.enewsletter.views import subscribe, subscribed
 
 urlpatterns = patterns('myproject.enewsletter.views',
-    url(r'^/subscribed/', 'subscribed'),
+    url(r'subscribed', 'subscribed'),
     url(r'', 'subscribe'),
 )
